5da98a5ddbee046c0ec11c89b2955f3e6794b227
TheLeanMonster <124800495+TheLeanMonster@users.noreply.github.com>
2023-03-16 10:50:48 -0700

    Add files via upload

---
+++ Content/Items/Misc/Radioactive_barrel.cs
@@ -0,0 +1,67 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using ProjectInfinity.Content.Bosses;
+using ProjectInfinity.Content.Bosses.RadioactiveSludge;
+
+namespace ProjectInfinity.Content.Items.Misc
+{
+    public class Radioactive_barrel : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Radioactive Barrel");
+            Tooltip.SetDefault("It attracts an abomination");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3;
+            ItemID.Sets.SortingPriorityBossSpawns[Type] = 12;
+
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = 20;
+            Item.value = 100;
+            Item.rare = ItemRarityID.Blue;
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.consumable = false;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+
+            return !NPC.AnyNPCs(ModContent.NPCType<Radioactive_sludge>());
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if (player.whoAmI == Main.myPlayer)
+            {
+
+                SoundEngine.PlaySound(SoundID.Roar, player.position);
+
+                int type = ModContent.NPCType<Radioactive_sludge>();
+
+                if (Main.netMode != NetmodeID.MultiplayerClient)
+                {
+                    NPC.SpawnOnPlayer(player.whoAmI, type);
+                }
+                else
+                {
+
+                    NetMessage.SendData(MessageID.SpawnBoss, number: player.whoAmI, number2: type);
+                }
+            }
+
+            return true;
+        }
+
+
+    }
+}
