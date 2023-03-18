491f5dcc96878e87c503f14f42bbf483c2b96407
AlphaLucy <toprak_sigirtmac@hotmail.com>
2023-03-18 22:10:17 +0300

    epic

--- Content/Projectiles/BaseProjectiles/LaserBase1.cs
+++ Content/Projectiles/BaseProjectiles/LaserBase1.cs
@@ -23,6 +23,6 @@ namespace ProjectInfinity.Content.Projectiles.BaseProjectiles
         public override bool friendly => true;
         public override bool hasMaxCharge => true;
         public override float MaxCharge => 120;
-        public override float MaxRange => 500;
+        public override float MaxRange => Main.player[Projectile.owner].Distance(Main.MouseWorld);
     }
 }
