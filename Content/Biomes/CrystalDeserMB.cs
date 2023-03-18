3e67630699c7abb11678c6ed35b99a220b442347
AlphaLucy <toprak_sigirtmac@hotmail.com>
2023-03-18 21:23:40 +0300

    deleted some annoying  checks

--- Content/Biomes/CrystalDeserMB.cs
+++ Content/Biomes/CrystalDeserMB.cs
@@ -22,13 +22,7 @@ namespace ProjectInfinity.Content.Biomes
         {
             // First, we will use the exampleBlockCount from our added ModSystem for our first custom condition
             bool b1 = ModContent.GetInstance<BiomeHandler>().CrystanSandstoneBlockCount >= 40;
-
-            // Second, we will limit this biome to the inner horizontal third of the map as our second custom condition
-            bool b2 = Math.Abs(player.position.ToTileCoordinates().X - Main.maxTilesX / 2) < Main.maxTilesX / 6;
-
-            // Finally, we will limit the height at which this biome can be active to above ground (ie sky and surface). Most (if not all) surface biomes will use this condition.
-            bool b3 = player.ZoneSkyHeight || player.ZoneOverworldHeight;
-            return b1 && b2 && b3 || player.ZoneRockLayerHeight || player.ZoneDirtLayerHeight;
+            return b1;
         }
         public override void OnInBiome(Player player)
         {
