21333028191403e61eb4b67830444113b2222b8c
AlphaLucy <toprak_sigirtmac@hotmail.com>
2023-03-18 21:31:24 +0300

    added dyeloader

---
+++ Core/Loaders/DyeLoader.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ProjectInfinity.Content.Items.Dye;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.Graphics.Effects;
+using Terraria.ModLoader;
+using ReLogic.Content;
+
+namespace ProjectInfinity.Core.Loaders
+{
+    internal class DyeLoader : IOrderedLoadable
+    {
+        public float Priority => 1.2f;
+        public void Load()
+        {
+            if (Main.dedServ)
+                return;
+        }
+        public void Unload()
+        {
+
+        }
+    }
+}
