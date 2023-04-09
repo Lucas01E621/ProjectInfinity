263cb6a7405ccf13d4640978f5884792bab7666e
AlphaLucy <toprak_sigirtmac@hotmail.com>
2023-04-10 01:35:53 +0300

    forgor the items :skull:

---
+++ Content/Items/Weapons/Summoner/CrystalDesert/CSsummoneritem2.cs
@@ -0,0 +1,16 @@
+﻿using ProjectInfinity.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ProjectInfinity.Content.Items.Weapons.Summoner.CrystalDesert
+{
+    internal class CSsummoneritem2 : ModItem
+    {
+        public override string Texture => AssetDirectory.Misc + "CrystalWand"; 
+    }
+}
---
+++ Content/Items/Weapons/Summoner/CrystalDesert/CrystalVortexStaff.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using ProjectInfinity.Content.Projectiles.CrystalDesert;
+using ProjectInfinity.Content.Projectiles.CrystalDesert.Weapons;
+using ProjectInfinity.Core;
+using System.Runtime.CompilerServices;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ProjectInfinity.Content.Items.Weapons.Summoner.CrystalDesert
+{
+    internal class CrystalVortexStaff : ModItem
+    {
+        public override string Texture => AssetDirectory.Misc + "CrystalWand";
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.damage = 25;
+            Item.ArmorPenetration = 15;
+            Item.DamageType = DamageClass.Summon;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.shoot = ProjectileID.PurificationPowder;
+            Item.shootSpeed = 10;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<CrystalVortex>(), Item.damage, knockback, Main.myPlayer);
+            return false;
+        }
+    }
+}
